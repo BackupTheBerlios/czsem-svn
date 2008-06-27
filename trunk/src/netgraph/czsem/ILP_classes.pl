5dc27c5e66d51aab93858b31b76cb714415f1a26
profesor <profesor@f6759c37-e42f-0410-80d9-d9952a3707de>
2008-06-27 08:39:41 +0000

    Pridana hlavicka s definicemi pro hledani pravidel.

--- trunk/src/netgraph/czsem/ILP_classes.pl
+++ trunk/src/netgraph/czsem/ILP_classes.pl
@@ -1,3 +1,59 @@
+%%%%%%%%%%%%%%%%%%%%%%%
+% Declarations
+%:- set(nodes,500000)?
+%:- set(r,10000)?
+%:- set(inflate,800000)?
+%:- set(posonly,1)?
+
+:- set(c,11),set(i,11)?
+:- set(verbose,1)?
+%:- set(noise,0.5)?
+:- set(h,2000)?
+
+:- modeh(1,injured(+const))?
+:- modeh(1,number_injured(+const, #numConst))?
+:- modeh(1,number_dead(+const, #numConst))?
+:- modeh(1,number_severe_injury(+const, #numConst))?
+:- modeb(*,edge(-node,+node))?
+:- modeb(*,edge(+node,-node))?
+:- modeb(*,edge(+node,+node))?
+:- modeb(1,id(+node,-const))?
+:- modeb(1,id(-node,+const))?
+%:- modeb(1,injured(+const))?
+%:- modeb(1,number_injured(+const, #numConst))?
+%:- modeb(1,number_dead(+const, #numConst))?
+%:- modeb(1,number_severe_injury(+const, #numConst))?
+
+
+:- modeb(1,m_tag0(+node,#const))?
+:- modeb(1,m_tag1(+node,#const))?
+:- modeb(1,m_tag2(+node,#const))?
+:- modeb(1,m_tag3(+node,#const))?
+:- modeb(1,m_tag4(+node,#const))?
+:- modeb(1,m_tag5(+node,#const))?
+:- modeb(1,m_tag6(+node,#const))?
+:- modeb(1,m_tag7(+node,#const))?
+:- modeb(1,m_tag8(+node,#const))?
+:- modeb(1,m_tag9(+node,#const))?
+:- modeb(1,m_tag10(+node,#const))?
+:- modeb(1,m_tag11(+node,#const))?
+:- modeb(1,m_tag12(+node,#const))?
+:- modeb(1,m_tag13(+node,#const))?
+:- modeb(1,m_tag14(+node,#const))?
+
+% begin of definitions of linguistic attributes
+:- modeb(1,functor(+node,#const))?
+:- modeb(1,gram_sempos(+node,#const))?
+:- modeb(1,id(+node,#const))?
+:- modeb(1,t_lemma(+node,#const))?
+:- modeb(1,a_afun(+node,#const))?
+:- modeb(1,m_form(+node,#const))?
+:- modeb(1,m_lemma(+node,#const))?
+:- modeb(1,m_tag(+node,#const))?
+% end of definitions of linguistic attributes
+
+
+
 injured(t_plzensky70827_txt_001_p1s3).
 injured(t_plzensky72450_txt_001_p3s2).
 injured(t_plzensky72450_txt_001_p19s2).
