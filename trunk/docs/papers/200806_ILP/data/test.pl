c804ddc8f0ccde3d1ec057434ecdec631b2a5f69
profesor <profesor@f6759c37-e42f-0410-80d9-d9952a3707de>
2008-06-27 13:43:33 +0000

    Přidána data, na kterých se získaly výsledky do článku.

---
+++ trunk/docs/papers/200806_ILP/data/test.pl
@@ -0,0 +1,44 @@
+injured(t_plzensky72675_txt_001_p7s2).
+injured(t_plzensky60661_txt_001_p1s2).
+injured(t_plzensky58576_txt_001_p5s2).
+injured(t_plzensky64131_txt_001_p2s2).
+injured(t_plzensky69695_txt_001_p1s1).
+injured(t_plzensky55831_txt_001_p1s3).
+injured(t_plzensky59722_txt_001_p2s3).
+
+injured(t_plzensky72261_txt_001_p14s2).
+injured(t_plzensky71760_txt_001_p2s7).
+injured(t_plzensky48663_txt_001_p2s2).
+
+
+injured(t_plzensky69691_txt_001_p2s4).
+injured(t_plzensky78857_txt_001_p1s7).
+injured(t_plzensky51802_txt_001_p6s2).
+injured(t_plzensky69694_txt_001_p3s4).
+injured(t_plzensky63162_txt_001_p1s3).
+
+:- injured(t_plzensky59722_txt_001_p2s7).
+:- injured(t_plzensky54670_txt_001_p1s3).
+:- injured(t_plzensky58468_txt_001_p1s6).
+:- injured(t_plzensky49288_txt_001_p6s6).
+:- injured(t_plzensky58468_txt_001_p1s4).
+:- injured(t_plzensky58468_txt_001_p1s5).
+:- injured(t_plzensky57010_txt_001_p3s3).
+:- injured(t_plzensky58576_txt_001_p4s3).
+:- injured(t_plzensky69460_txt_001_p1s7).
+:- injured(t_plzensky49244_txt_001_p1s2).
+:- injured(t_plzensky72928_txt_001_p1s6).
+:- injured(t_plzensky58468_txt_001_p1s8).
+:- injured(t_plzensky81415_txt_001_p1s1).
+:- injured(t_plzensky79543_txt_001_p1s1).
+:- injured(t_plzensky79543_txt_001_p1s2).
+
+:- injured(t_plzensky72261_txt_001_p15s5).
+:- injured(t_plzensky51278_txt_001_p5s3).
+:- injured(t_plzensky57806_txt_001_p2s40).
+:- injured(t_plzensky51278_txt_001_p5s4).
+:- injured(t_plzensky51278_txt_001_p5s5).
+:- injured(t_plzensky51278_txt_001_p5s6).
+:- injured(t_plzensky51278_txt_001_p5s7).
+:- injured(t_plzensky51278_txt_001_p5s8).
+
---
+++ trunk/docs/papers/200806_ILP/data/testNumbers.pl
@@ -0,0 +1,180 @@
+
+:- number_injured(t_plzensky70827_txt_001_p1s3,1).
+:- number_injured(t_plzensky70827_txt_001_p1s3,3).
+:- number_injured(t_plzensky70827_txt_001_p1s3,4).
+:- number_injured(t_plzensky70827_txt_001_p1s3,5).
+:- number_injured(t_plzensky70827_txt_001_p1s3,6).
+:- number_injured(t_plzensky70827_txt_001_p1s3,7).
+:- number_injured(t_plzensky70827_txt_001_p1s3,8).
+:- number_injured(t_plzensky70827_txt_001_p1s3,9).
+number_injured(t_plzensky70827_txt_001_p1s3,2).
+:- number_injured(t_plzensky72450_txt_001_p3s2,2).
+:- number_injured(t_plzensky72450_txt_001_p3s2,3).
+:- number_injured(t_plzensky72450_txt_001_p3s2,4).
+:- number_injured(t_plzensky72450_txt_001_p3s2,5).
+:- number_injured(t_plzensky72450_txt_001_p3s2,6).
+:- number_injured(t_plzensky72450_txt_001_p3s2,7).
+:- number_injured(t_plzensky72450_txt_001_p3s2,8).
+:- number_injured(t_plzensky72450_txt_001_p3s2,9).
+number_injured(t_plzensky72450_txt_001_p3s2,1).
+:- number_injured(t_plzensky72450_txt_001_p19s2,2).
+:- number_injured(t_plzensky72450_txt_001_p19s2,3).
+:- number_injured(t_plzensky72450_txt_001_p19s2,4).
+:- number_injured(t_plzensky72450_txt_001_p19s2,5).
+:- number_injured(t_plzensky72450_txt_001_p19s2,6).
+:- number_injured(t_plzensky72450_txt_001_p19s2,7).
+:- number_injured(t_plzensky72450_txt_001_p19s2,8).
+:- number_injured(t_plzensky72450_txt_001_p19s2,9).
+number_injured(t_plzensky72450_txt_001_p19s2,1).
+:- number_injured(t_plzensky63162_txt_001_p1s3,2).
+:- number_injured(t_plzensky63162_txt_001_p1s3,3).
+:- number_injured(t_plzensky63162_txt_001_p1s3,4).
+:- number_injured(t_plzensky63162_txt_001_p1s3,5).
+:- number_injured(t_plzensky63162_txt_001_p1s3,6).
+:- number_injured(t_plzensky63162_txt_001_p1s3,7).
+:- number_injured(t_plzensky63162_txt_001_p1s3,8).
+:- number_injured(t_plzensky63162_txt_001_p1s3,9).
+number_injured(t_plzensky63162_txt_001_p1s3,1).
+:- number_injured(t_plzensky72795_txt_001_p2s3,2).
+:- number_injured(t_plzensky72795_txt_001_p2s3,3).
+:- number_injured(t_plzensky72795_txt_001_p2s3,4).
+:- number_injured(t_plzensky72795_txt_001_p2s3,5).
+:- number_injured(t_plzensky72795_txt_001_p2s3,6).
+:- number_injured(t_plzensky72795_txt_001_p2s3,7).
+:- number_injured(t_plzensky72795_txt_001_p2s3,8).
+:- number_injured(t_plzensky72795_txt_001_p2s3,9).
+number_injured(t_plzensky72795_txt_001_p2s3,1).
+:- number_injured(t_plzensky60375_txt_001_p1s6,2).
+:- number_injured(t_plzensky60375_txt_001_p1s6,3).
+:- number_injured(t_plzensky60375_txt_001_p1s6,4).
+:- number_injured(t_plzensky60375_txt_001_p1s6,5).
+:- number_injured(t_plzensky60375_txt_001_p1s6,6).
+:- number_injured(t_plzensky60375_txt_001_p1s6,7).
+:- number_injured(t_plzensky60375_txt_001_p1s6,8).
+:- number_injured(t_plzensky60375_txt_001_p1s6,9).
+number_injured(t_plzensky60375_txt_001_p1s6,1).
+
+
+
+:- number_dead(t_plzensky60375_txt_001_p1s8,2).
+:- number_dead(t_plzensky60375_txt_001_p1s8,3).
+:- number_dead(t_plzensky60375_txt_001_p1s8,4).
+:- number_dead(t_plzensky60375_txt_001_p1s8,5).
+:- number_dead(t_plzensky60375_txt_001_p1s8,6).
+:- number_dead(t_plzensky60375_txt_001_p1s8,7).
+:- number_dead(t_plzensky60375_txt_001_p1s8,8).
+:- number_dead(t_plzensky60375_txt_001_p1s8,9).
+number_dead(t_plzensky60375_txt_001_p1s8,1).
+:- number_dead(t_plzensky72977_txt_001_p1s2,2).
+:- number_dead(t_plzensky72977_txt_001_p1s2,3).
+:- number_dead(t_plzensky72977_txt_001_p1s2,4).
+:- number_dead(t_plzensky72977_txt_001_p1s2,5).
+:- number_dead(t_plzensky72977_txt_001_p1s2,6).
+:- number_dead(t_plzensky72977_txt_001_p1s2,7).
+:- number_dead(t_plzensky72977_txt_001_p1s2,8).
+:- number_dead(t_plzensky72977_txt_001_p1s2,9).
+number_dead(t_plzensky72977_txt_001_p1s2,1).
+:- number_dead(t_plzensky48663_txt_001_p2s2,1).
+:- number_dead(t_plzensky48663_txt_001_p2s2,3).
+:- number_dead(t_plzensky48663_txt_001_p2s2,4).
+:- number_dead(t_plzensky48663_txt_001_p2s2,5).
+:- number_dead(t_plzensky48663_txt_001_p2s2,6).
+:- number_dead(t_plzensky48663_txt_001_p2s2,7).
+:- number_dead(t_plzensky48663_txt_001_p2s2,8).
+:- number_dead(t_plzensky48663_txt_001_p2s2,9).
+number_dead(t_plzensky48663_txt_001_p2s2,2).
+:- number_dead(t_plzensky61718_txt_001_p3s3,1).
+:- number_dead(t_plzensky61718_txt_001_p3s3,2).
+:- number_dead(t_plzensky61718_txt_001_p3s3,3).
+:- number_dead(t_plzensky61718_txt_001_p3s3,4).
+:- number_dead(t_plzensky61718_txt_001_p3s3,5).
+:- number_dead(t_plzensky61718_txt_001_p3s3,6).
+:- number_dead(t_plzensky61718_txt_001_p3s3,7).
+:- number_dead(t_plzensky61718_txt_001_p3s3,8).
+:- number_dead(t_plzensky61718_txt_001_p3s3,9).
+number_dead(t_plzensky61718_txt_001_p3s3,333).
+:- number_dead(t_plzensky57870_txt_001_p1s1,1).
+:- number_dead(t_plzensky57870_txt_001_p1s1,3).
+:- number_dead(t_plzensky57870_txt_001_p1s1,4).
+:- number_dead(t_plzensky57870_txt_001_p1s1,5).
+:- number_dead(t_plzensky57870_txt_001_p1s1,6).
+:- number_dead(t_plzensky57870_txt_001_p1s1,7).
+:- number_dead(t_plzensky57870_txt_001_p1s1,8).
+:- number_dead(t_plzensky57870_txt_001_p1s1,9).
+number_dead(t_plzensky57870_txt_001_p1s1,2).
+
+
+
+:- number_severe_injury(t_plzensky48663_txt_001_p2s2,1).
+:- number_severe_injury(t_plzensky48663_txt_001_p2s2,3).
+:- number_severe_injury(t_plzensky48663_txt_001_p2s2,4).
+:- number_severe_injury(t_plzensky48663_txt_001_p2s2,5).
+:- number_severe_injury(t_plzensky48663_txt_001_p2s2,6).
+:- number_severe_injury(t_plzensky48663_txt_001_p2s2,7).
+:- number_severe_injury(t_plzensky48663_txt_001_p2s2,8).
+:- number_severe_injury(t_plzensky48663_txt_001_p2s2,9).
+number_severe_injury(t_plzensky48663_txt_001_p2s2,2).
+:- number_severe_injury(t_plzensky61718_txt_001_p3s3,1).
+:- number_severe_injury(t_plzensky61718_txt_001_p3s3,2).
+:- number_severe_injury(t_plzensky61718_txt_001_p3s3,3).
+:- number_severe_injury(t_plzensky61718_txt_001_p3s3,4).
+:- number_severe_injury(t_plzensky61718_txt_001_p3s3,5).
+:- number_severe_injury(t_plzensky61718_txt_001_p3s3,6).
+:- number_severe_injury(t_plzensky61718_txt_001_p3s3,7).
+:- number_severe_injury(t_plzensky61718_txt_001_p3s3,8).
+:- number_severe_injury(t_plzensky61718_txt_001_p3s3,9).
+number_severe_injury(t_plzensky61718_txt_001_p3s3,333).
+:- number_severe_injury(t_plzensky58020_txt_001_p1s2,2).
+:- number_severe_injury(t_plzensky58020_txt_001_p1s2,3).
+:- number_severe_injury(t_plzensky58020_txt_001_p1s2,4).
+:- number_severe_injury(t_plzensky58020_txt_001_p1s2,5).
+:- number_severe_injury(t_plzensky58020_txt_001_p1s2,6).
+:- number_severe_injury(t_plzensky58020_txt_001_p1s2,7).
+:- number_severe_injury(t_plzensky58020_txt_001_p1s2,8).
+:- number_severe_injury(t_plzensky58020_txt_001_p1s2,9).
+number_severe_injury(t_plzensky58020_txt_001_p1s2,1).
+:- number_severe_injury(t_plzensky58020_txt_001_p1s4,2).
+:- number_severe_injury(t_plzensky58020_txt_001_p1s4,3).
+:- number_severe_injury(t_plzensky58020_txt_001_p1s4,4).
+:- number_severe_injury(t_plzensky58020_txt_001_p1s4,5).
+:- number_severe_injury(t_plzensky58020_txt_001_p1s4,6).
+:- number_severe_injury(t_plzensky58020_txt_001_p1s4,7).
+:- number_severe_injury(t_plzensky58020_txt_001_p1s4,8).
+:- number_severe_injury(t_plzensky58020_txt_001_p1s4,9).
+number_severe_injury(t_plzensky58020_txt_001_p1s4,1).
+:- number_severe_injury(t_plzensky57870_txt_001_p1s1,1).
+:- number_severe_injury(t_plzensky57870_txt_001_p1s1,3).
+:- number_severe_injury(t_plzensky57870_txt_001_p1s1,4).
+:- number_severe_injury(t_plzensky57870_txt_001_p1s1,5).
+:- number_severe_injury(t_plzensky57870_txt_001_p1s1,6).
+:- number_severe_injury(t_plzensky57870_txt_001_p1s1,7).
+:- number_severe_injury(t_plzensky57870_txt_001_p1s1,8).
+:- number_severe_injury(t_plzensky57870_txt_001_p1s1,9).
+number_severe_injury(t_plzensky57870_txt_001_p1s1,2).
+:- number_severe_injury(t_plzensky60375_txt_001_p1s8,2).
+:- number_severe_injury(t_plzensky60375_txt_001_p1s8,3).
+:- number_severe_injury(t_plzensky60375_txt_001_p1s8,4).
+:- number_severe_injury(t_plzensky60375_txt_001_p1s8,5).
+:- number_severe_injury(t_plzensky60375_txt_001_p1s8,6).
+:- number_severe_injury(t_plzensky60375_txt_001_p1s8,7).
+:- number_severe_injury(t_plzensky60375_txt_001_p1s8,8).
+:- number_severe_injury(t_plzensky60375_txt_001_p1s8,9).
+number_severe_injury(t_plzensky60375_txt_001_p1s8,1).
+:- number_severe_injury(t_plzensky72977_txt_001_p1s2,2).
+:- number_severe_injury(t_plzensky72977_txt_001_p1s2,3).
+:- number_severe_injury(t_plzensky72977_txt_001_p1s2,4).
+:- number_severe_injury(t_plzensky72977_txt_001_p1s2,5).
+:- number_severe_injury(t_plzensky72977_txt_001_p1s2,6).
+:- number_severe_injury(t_plzensky72977_txt_001_p1s2,7).
+:- number_severe_injury(t_plzensky72977_txt_001_p1s2,8).
+:- number_severe_injury(t_plzensky72977_txt_001_p1s2,9).
+number_severe_injury(t_plzensky72977_txt_001_p1s2,1).
+:- number_severe_injury(t_plzensky71760_txt_001_p2s7,1).
+:- number_severe_injury(t_plzensky71760_txt_001_p2s7,3).
+:- number_severe_injury(t_plzensky71760_txt_001_p2s7,4).
+:- number_severe_injury(t_plzensky71760_txt_001_p2s7,5).
+:- number_severe_injury(t_plzensky71760_txt_001_p2s7,6).
+:- number_severe_injury(t_plzensky71760_txt_001_p2s7,7).
+:- number_severe_injury(t_plzensky71760_txt_001_p2s7,8).
+:- number_severe_injury(t_plzensky71760_txt_001_p2s7,9).
+number_severe_injury(t_plzensky71760_txt_001_p2s7,2).
